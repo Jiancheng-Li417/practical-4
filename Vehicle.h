d9454cc8ee568dfab6d07706d2200c6f6c05dbf5
Jiancheng-Li417 <15343519972@163.com>
2024-08-30 22:01:31 +0930

    Add files via upload

---
+++ Vehicle.h
@@ -0,0 +1,18 @@
+#ifndef VEHICLE_H  
+#define VEHICLE_H  
+
+#include <ctime>  
+
+class Vehicle {
+protected:
+    std::time_t timeOfEntry;
+    int id;
+
+public:
+    Vehicle(int id);
+    int getID() const;
+    virtual int getParkingDuration(std::time_t currentTime) const = 0;
+    virtual ~Vehicle();
+};
+
+#endif
